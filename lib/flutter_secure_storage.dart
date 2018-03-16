0b8d368605cfa2d8cc5509d18f56825396aa5e4a
Evo Stamatov <aviolito@gmail.com>
2018-03-16 11:47:22 +1100

    breaking: removed keyed params
    added argument checks
    reformatted with `flutter format`

--- lib/flutter_secure_storage.dart
+++ lib/flutter_secure_storage.dart
@@ -5,20 +5,24 @@ import 'package:meta/meta.dart';
 
 class FlutterSecureStorage {
   static const MethodChannel _channel =
-  const MethodChannel('plugins.it_nomads.com/flutter_secure_storage');
+      const MethodChannel('plugins.it_nomads.com/flutter_secure_storage');
 
-  Future write({@required String key, @required String value}) =>
-      _channel.invokeMethod("write", <String, String>{
-        "key": key,
-        "value": value});
+  Future<Null> write(String key, String value) {
+    if (key == null) throw new ArgumentError.notNull('key');
+    if (value == null) throw new ArgumentError.notNull('value');
+    return _channel.invokeMethod('write', {
+      'key': key,
+      'value': value,
+    });
+  }
 
-  Future read({@required String key}) =>
-      _channel.invokeMethod("read", <String, String>{
-        "key": key
-      });
+  Future<String> read(String key) {
+    if (key == null) throw new ArgumentError.notNull('key');
+    return _channel.invokeMethod('read', {'key': key});
+  }
 
-  Future delete({@required String key}) =>
-      _channel.invokeMethod("delete", <String, String>{
-        "key": key
-      });
+  Future<Null> delete(String key) {
+    if (key == null) throw new ArgumentError.notNull('key');
+    return _channel.invokeMethod('delete', {'key': key});
+  }
 }
